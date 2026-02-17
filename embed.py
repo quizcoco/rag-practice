c8ba1df9492cc19b490a63dadef08e5230f7ec7a
quizcoco <k.yuna.dev@gmail.com>
2026-02-18 04:57:40 +0900

    backup: chunks.json사용한 dict 기반 RAG 구조

--- embed.py
+++ embed.py
@@ -11,9 +11,6 @@ model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-MiniL
 with open("chunks.json", "r", encoding="utf-8") as f:
     chunks = json.load(f)
 
-
-# texts = [c["text"] for c in chunks]
-# titles = [c["title"] for c in chunks]
 ids = [c["id"] for c in chunks]
 
 contents = [
@@ -21,14 +18,8 @@ contents = [
     for c in chunks
 ]
 
-embeddings = model.encode(contents, normalize_embeddings=True) # 정규화된 벡터?
+embeddings = model.encode(contents, normalize_embeddings=True) # 정규화된 벡터
 
-# print(type(embeddings))
-# print(type(embeddings[0]))
-# print(len(embeddings[0]))
-# print(embeddings[0][:10])  # 앞부분만
-
-# print(cosine_similarity([embeddings[0]], [embeddings[1]]))
 
 metadatas = []
 
@@ -41,7 +32,7 @@ for c in chunks:
     metadatas.append(meta)
 
 client = chromadb.Client()
-#collection = client.create_collection(name="chunks_docs")
+
 collection = client.get_or_create_collection(name="chunks_docs")
 
 if collection.count() == 0:
@@ -51,9 +42,6 @@ if collection.count() == 0:
         ids=ids,
         metadatas=metadatas
     )
-
-# print(collection.count())
-# print(collection.peek())
 
 
 # 앵커의 children 리스트
@@ -67,7 +55,6 @@ for c in chunks:
 
 def search(query: str, k_anchor: int = 1, k_chunk: int = 7):
 
-    # query = "osi의 역사는 어떻게 진행되나요?"
     query_embedding = model.encode([query], normalize_embeddings=True)
 
     anchor_results = collection.query( #유사한 것 찾기
@@ -75,7 +62,7 @@ def search(query: str, k_anchor: int = 1, k_chunk: int = 7):
         n_results=k_anchor,
         where={"type":"anchor"}
     )
-    # print(anchor_results)
+    
       # 가장 관련 있는 앵커 ID 가져오기
 
     if not anchor_results["ids"][0]:
@@ -117,20 +104,5 @@ def search(query: str, k_anchor: int = 1, k_chunk: int = 7):
     
     # 첫 번째 청크 반환
 
-    # return results["documents"][0]
 
 search("osi 역사??")
-
-
-# for i, doc in enumerate(results["documents"][0]):
-#     print(f"\n[{i+1}번 결과]")
-#     print(doc)
-
-###임베딩 저장####
-
-# import numpy as np
-
-# np.save("embeddings.npy", embeddings)
-# loaded = np.load("embeddings.npy")
-
-# print(np.allclose(embeddings[0], loaded[0])) # True
